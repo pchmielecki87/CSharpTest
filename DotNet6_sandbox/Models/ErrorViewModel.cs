44d8fa049fecb9c44f57da38a8963af6916a9025
pchmielecki87 <pchmielecki87@gmail.com>
2021-11-10 10:30:56 +0100

    cleaning

---
+++ DotNet6_sandbox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet6_sandbox.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
