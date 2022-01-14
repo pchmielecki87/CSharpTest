06070ae8888dca19497df37af1b3a3e5e4c40272
pchmielecki87 <pchmielecki87@gmail.com>
2022-01-14 14:50:46 +0100

    api update

--- con_Api1/Controllers/PizzaController.cs
+++ con_Api1/Controllers/PizzaController.cs
@@ -13,15 +13,55 @@ namespace ContosoPizza.Controllers
         public PizzaController()
         {
         }
+
         // GET all action
-        
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
         // GET by Id action
-        
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza == null)
+                return NotFound();
+            return pizza;
+        }
+
         // POST action
-        
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+
         // PUT action
-        
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+            PizzaService.Update(pizza);
+            return NoContent();
+        }
+
         // DELETE action
-        
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza is null)
+                return NotFound();
+            PizzaService.Delete(id);
+            return NoContent();
+        }
+
+
     }
 }
