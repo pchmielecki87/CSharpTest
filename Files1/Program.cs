eb1f94cfa433d525a23e56d032ef34f10c53434e
pchmielecki87 <pchmielecki87@gmail.com>
2022-01-14 10:24:30 +0100

    initial project

---
+++ Files1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace files_module{
+    class Program{
+        static void Main(string[] args){
+            var files = FindFiles("stores");
+foreach(var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+static IEnumerable<string> FindFiles(string folderName){
+            List<string> salesFiles = new List<string>();
+    
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    
+            foreach(var file in foundFiles)
+            {
+                if(file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+    
+            return salesFiles;
+        }
+    }
+}
