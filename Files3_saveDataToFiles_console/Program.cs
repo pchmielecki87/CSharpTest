a8b66eaf567745a96324f892e26b8c1b4f8518a9
pchmielecki87 <pchmielecki87@gmail.com>
2022-01-14 10:49:14 +0100

    update files3

---
+++ Files3_saveDataToFiles_console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace files_module{
+    class Program{
+        static void Main(string[] args){
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+var salesFiles = FindFiles(storesDirectory);
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+        }
+
+static IEnumerable<string> FindFiles(string folderName){
+            List<string> salesFiles = new List<string>();
+var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+foreach(var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+if(extension == ".json")
+                {
+                    salesFiles.Add(file);
+                }
+            }
+return salesFiles;
+        }
+    }
+}
