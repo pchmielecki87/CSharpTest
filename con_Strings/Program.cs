96398b3ba74a40e2366025d3effa4fdcf1cc2277
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2022-07-22 09:16:52 +0200

    work with strings

---
+++ con_Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace con_String
+  {
+    class Program
+      {
+        static void Main(string[] args)
+          {
+            Console.Write("Type the year of your birth: ");
+            string userInput = Console.ReadLine();
+            int yearOfBirth;
+            int.TryParse(userInput, out yearOfBirth);
+
+            int age = DateTime.Now.Year - yearOfBirth;
+
+            Console.WriteLine("Your age is " + age);
+          }
+      }
+}
